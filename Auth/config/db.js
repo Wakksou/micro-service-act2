0da3dacfb0a2ba84981024c406ae7e0753809f41
MaximeDngProject <max94220@hotmail.fr>
2025-07-08 16:39:45 +0200

    .env

--- Auth/config/db.js
+++ Auth/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URI, {
-    ssl: process.env.MONGO_SSL === 'true'
-  })
+  mongoose.connect(process.env.MONGO_URI, {ssl: process.env.MONGO_SSL})
   .then(() => console.log("Connexion à MongoDB réussie"))
   .catch((err) => {
     console.error("Erreur MongoDB :", err);
