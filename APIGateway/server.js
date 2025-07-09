f1ed705a38f9b21de57f4404dc78f33dadf15a20
MaximeDngProject <max94220@hotmail.fr>
2025-07-09 14:15:30 +0200

    docker

--- APIGateway/server.js
+++ APIGateway/server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const proxy = require('express-http-proxy');
 const app = express();
 
-app.use('/api/auth', proxy('http://localhost:8081'));
+// app.use('/api/auth', proxy('http://localhost:8081'));
+app.use('/api/auth', proxy(process.env.AUTH_URL));
 
-const PORT = 3000;
+// const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`🌐 API Gateway en écoute sur http://localhost:${PORT}`);
 });
